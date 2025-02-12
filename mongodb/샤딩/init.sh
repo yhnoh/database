feb0831fff05422334d4198df6c77539c617d874
yhnoh <tedgom19@gmail.com>
2025-02-12 22:48:49 +0900

    mongodb 샤딩 기본 구성

--- mongodb/샤딩/init.sh
+++ mongodb/샤딩/init.sh
@@ -1,3 +1,5 @@
 docker exec config_mongodb1 mongosh /script/init.js
 docker exec shard_mongodb1 mongosh /script/init.js
 docker exec mongos_mongodb mongosh /script/init.js
+
+docker exec shard_mongodb1 mongosh /script/data.js
