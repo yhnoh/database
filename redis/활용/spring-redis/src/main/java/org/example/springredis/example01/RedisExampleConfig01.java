c246e634ba4026d71e03370e5e9f6e3f76ace804
yhnoh <tedgom19@gmail.com>
2025-05-10 15:57:10 +0900

    인기 검색어 제작

--- redis/활용/spring-redis/src/main/java/org/example/springredis/example01/RedisExampleConfig01.java
+++ redis/활용/spring-redis/src/main/java/org/example/springredis/example01/RedisExampleConfig01.java
@@ -1,9 +1,17 @@
 package org.example.springredis.example01;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.BoundZSetOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Set;
+import java.util.TreeSet;
 
 
 /**
@@ -20,6 +28,33 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 public class RedisExampleConfig01 {
 
     private final StringRedisTemplate stringRedisTemplate;
+
+    @PostConstruct
+    public void postConstruct() {
+
+        ZonedDateTime now = ZonedDateTime.now(ZoneId.systemDefault());
+        ZonedDateTime beforeOneHour = now.minusHours(1);
+        this.setRandomData(new RedisSearchKeywordKey(now).getKey(), 20);
+        this.setRandomData(new RedisSearchKeywordKey(beforeOneHour).getKey(), 20);
+    }
+
+    /**
+     * 테스트 데이터 셋팅
+     */
+    public void setRandomData(String key, int count) {
+        BoundZSetOperations<String, String> sortedSet = stringRedisTemplate.boundZSetOps(key);
+        Set<ZSetOperations.TypedTuple<String>> tuples = new TreeSet<>();
+        for (int i = 1; i <= count; i++) {
+            //0 ~ 100 사이의 데이터 생성
+            String value = "keyword_" + i;
+            int score = (int) (Math.random() * 100) + 1;
+
+            ZSetOperations.TypedTuple<String> tuple = ZSetOperations.TypedTuple.of(value, (double) score);
+            tuples.add(tuple);
+        }
+
+        sortedSet.add(tuples);
+    }
 
     @Bean
     public RedisExampleService01 redisExampleService() {
